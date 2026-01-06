9798c65e4eeadd2ad076d7f9ffb06321b52f5356
Vitor <vitor@example.com>
2026-01-06 10:23:07 -0600

    Fix: Add asset routes and improve Vite config for production build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    sourcemap: false
-  }
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: false,
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  },
+  publicDir: 'public'
 })
